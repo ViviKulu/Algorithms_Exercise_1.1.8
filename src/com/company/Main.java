ce51ef19c8c2541b30903a4a6f3d00721bc575b8
Vivian Babirye <babimaji1111@gmail.com>
2018-12-20 00:19:59 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+        //    What do each of the following print?
+
+        //    a. System.out.println('b');
+        //    b. System.out.println('b' + 'c');
+        //    c. System.out.println((char) ('a' + 4));
+
+        //    Explain each outcome.
+
+        //    a = b
+        //    b = 197
+        //    c = e
+
+    public static void main(String[] args) {
+        // write your code here
+
+    }
+}
